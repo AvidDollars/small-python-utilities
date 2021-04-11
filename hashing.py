a51e6e5772ab7498ec1544dce71d47025bf8a151
Dimitrij Kolničenko <dimitij.kol@protonmail.com>
2021-04-11 12:48:08 +0200

    added hashing.py module

---
+++ hashing.py
@@ -0,0 +1,13 @@
+import hashlib
+
+def hash(string, *, algorithm="sha256"):
+    """
+    returns a hash (=digest) of a provided string
+    default algorithm is sha256
+    """
+    algo = getattr(hashlib, algorithm, None)
+    if algo is None:
+        raise AttributeError(f"Algorithm '{algorithm}' does not exist")
+    m = algo()
+    m.update(bytearray(string, encoding="utf-8"))
+    return m.hexdigest()
